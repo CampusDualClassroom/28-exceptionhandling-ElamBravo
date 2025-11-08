2686922ed3618545b10e98554d21f13dc4dad07d
Elam Bravo <bravoelam@gmail.com>
2025-11-08 21:28:51 +0100

    Solved Exercise 28

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,12 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
-
+        try {
+            int resultado = 10 / 0;
+            System.out.println("Resultado: " + resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Se ha producido una excepción: " + e.getMessage());
+        }
     }
 }
+
